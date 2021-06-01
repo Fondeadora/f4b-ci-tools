e4aed45421f9d80657686bd4d7547ebf1633d49d
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2021-06-01 13:46:57 -0500

    Update deploy.sh

--- common/docker/deploy.sh
+++ common/docker/deploy.sh
@@ -9,7 +9,7 @@ STACK_FILE=$1
 TAG_NAME=$2
 APP_NAME=$3
 APP_PORT=$4
-REPOSITORY=$5
+AWS_ACCOUNT=$5
 CLUSTER=$6
 AWS_KEY=$7
 AWS_SECRET=$8
@@ -19,6 +19,7 @@ export AWS_ACCESS_KEY_ID=$AWS_KEY
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET
 export AWS_DEFAULT_REGION=$AWS_REGION
 
+REPOSITORY="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
 
 aws cloudformation update-stack --stack-name $APP_NAME \
   --template-body file:///$STACK_FILE --parameters \
