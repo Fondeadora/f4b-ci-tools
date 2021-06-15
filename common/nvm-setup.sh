c2dfc3cfe069e766e4f54af8878d6b00b680b4fc
gonz <gonzasestopal@gmail.com>
2021-06-15 11:40:36 -0500

    chore: support for .env
    feat: add nvm to chose node running version

---
+++ common/nvm-setup.sh
@@ -0,0 +1,6 @@
+
+sudo wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# export NVM_DIR="$HOME/.nvm"
+# sudo [ -s "$NVM_DIR/nvm.sh" ] && sudo \. "$NVM_DIR/nvm.sh"
+# sudo nvm install $NODE_VERSION && sudo nvm use $NODE_VERSION
