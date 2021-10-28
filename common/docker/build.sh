b74295164419bf13f6a85f2db097181b3da4b94c
Eduardo Aguilar <dante.aguilar41@gmail.com>
2021-10-27 21:47:00 -0500

    feat: Add docker workers configuration for gunicorn based applications

--- common/docker/build.sh
+++ common/docker/build.sh
@@ -6,6 +6,7 @@ APP_PORT=$3
 AWS_KEY=$4
 AWS_REGION=$5
 AWS_SECRET=$6
+WORKERS=$7
 
 docker build -t $APP_NAME:latest \
   --build-arg stage=$STAGE \
@@ -13,4 +14,5 @@ docker build -t $APP_NAME:latest \
   --build-arg aws_key=$AWS_KEY \
   --build-arg aws_secret=$AWS_SECRET \
   --build-arg aws_region=$AWS_REGION \
+  --build-arg workers=$WORKERS \
   .
