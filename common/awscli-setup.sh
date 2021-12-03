375bf84d39cc646a427e2eb9cfc7b034c8d566b1
gonz <gonzasestopal@gmail.com>
2021-12-03 13:59:02 -0600

    chore: bump aws cli

--- common/awscli-setup.sh
+++ common/awscli-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.4.4.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 rm -rf aws awscliv2.zip
